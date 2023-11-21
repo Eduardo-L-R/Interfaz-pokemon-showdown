c40405f7bafc7898357460f37630c907368fb138
John Asto <john.azt.dev@gmail.com>
2023-11-20 23:44:58 -0300

    add image with next.config domains

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,24 @@ export default function Home() {
 		<main className='flex min-h-screen flex-col items-center p-24'>
 			<div>
 				<Image
-					className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-					src='/next.svg'
+					className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mb-10'
+					src='https://pbs.twimg.com/media/FJWC5nCWYAQkjS0.png'
 					alt='Next.js Logo'
-					width={180}
-					height={37}
+					width={200}
+					height={100}
 					priority
 				/>
 			</div>
-			<h3>Interfaz Component Showdown</h3>
+			<h3 className='mb-4'>Interfaz Component Showdown</h3>
+			<form>
+				<div className='flex gap-4'>
+					<input
+						id='legend'
+						type='checkbox'
+					/>
+					<label htmlFor='legend'>Legend</label>
+				</div>
+			</form>
 		</main>
 	);
 }
